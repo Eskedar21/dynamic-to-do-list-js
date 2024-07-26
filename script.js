895a9c9268b1241c2afeb07783f93f616b3c2dea
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-07-26 18:50:51 +0300

    todo_list

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function(){
+    //Select DOM Elements
+  const addButton = document.getElementById('add-task-btn');
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+   //Create the addTask Function
+  addTask = () =>  {
+     let taskText = taskInput.value.trim();
+     if( taskInput === " "){
+        alert('Please Input Your Task')
+     } else {
+
+        //Task Creation and Removal
+        let list = document.createElement('li')
+        list.textContent = taskText;
+        let removeBtn = document.createElement('button')
+        removeBtn.textContent = "Remove";
+        removeBtn.className = 'remove-btn';
+
+        // Assign an onclick event to the remove button
+        removeBtn.onclick = function() {
+            taskList.removeChild(list);
+        };
+
+        // Append the remove button to the <li> element
+        list.appendChild(removeBtn);
+
+        // Append the <li> element to taskList
+        taskList.appendChild(list);
+
+        // Clear the task input field
+        taskInput.value = '';
+     }
+
+      //Attach Event Listeners
+       addButton.addEventListener('click', addTask);
+       taskInput.addEventListener('keypress', function(event){
+        if (event.key === 'Enter') {
+            addTask();
+        }
+     })
+  }
+  
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    addTask();
+})
